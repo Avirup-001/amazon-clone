0087e2f3b6628bedf59bc47e472616c3beab91ac
Avirup Banik <avirup.banik2017@gmail.com>
2022-06-19 00:30:50 +0530

    Updated product.css & some other designs

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -78,11 +78,12 @@ function Home() {
                 />
             <Product
                 id="49538094"
-                title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+                title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk"
                 price={239.0}
                 rating={4}
                 image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
                 />
+                
             </div>
 
         <div className='home__row'>
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -25,7 +25,7 @@ function Product({ id, title, image, price, rating }) {
   return (
     <div className='product'>
         <div className="product__info">
-        <p>{title}</p>
+          <p>{title}</p>
         <p className="product__price">
           <small>$</small>
           <strong>{price}</strong>
@@ -33,10 +33,8 @@ function Product({ id, title, image, price, rating }) {
         <div className="product__rating">
             <Rating name="half-rating-read" defaultValue={rating} precision={0.1} readOnly />
         </div>
-      </div>
-
-      <img src={image} alt="" />
-
+        </div>
+        <img src={image} alt="" />
       <button onClick={addToBasket}>Add to Basket</button>
     </div>
   )
